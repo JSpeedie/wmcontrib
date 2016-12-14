f30c4ca083674641b3bbed04a4f1005da41c822c
JSpeedie <julianspeedie@gmail.com>
2016-12-14 20:54:10 +0000

    Began switch to getopt instead of freeform arguments. Began work on new options (abNm). Redesigned to use screen ratios rather than 9 defined corners. Gives user more options, also simplifies get_coord_for_corner quite a bit
    Former-commit-id: 79f607370a2df401cd3250226e80a8a0b7f0348b

--- wmvc.c
+++ wmvc.c
@@ -2,6 +2,7 @@
 #define _XOPEN_SOURCE 500
 
 #include <err.h>
+#include <getopt.h>
 #include <limits.h>
 #include <math.h>
 #include <stdio.h>
@@ -16,27 +17,14 @@ Display *dpy;
 int focus = 0;
 int compatible = 0;
 
-/* Constants to represent the corners of the screen */
-static const int TOP_LEFT = 0;
-static const int TOP_CENTER = 1;
-static const int TOP_RIGHT = 2;
-static const int MIDDLE_LEFT = 3;
-static const int MIDDLE_CENTER = 4;
-static const int MIDDLE_RIGHT = 5;
-static const int BOTTOM_LEFT = 6;
-static const int BOTTOM_CENTER = 7;
-static const int BOTTOM_RIGHT = 8;
-
-
 void print_help(void) {
 	printf("usage: wmvc <corner> <wid>\n");
-	exit(0);
 }
 
 int window_exists(Window win) {
 
 	Display *dpy;
-	dpy = XOpenDisplay(":0");
+	dpy = XOpenDisplay(0);
 	int screen = DefaultScreen(dpy);
 	Window root_win = RootWindow(dpy, screen);
 	Window root_ret, parent_ret, *child_ret;
@@ -63,7 +51,7 @@ int get_screen_number_of_win(Window win, Display *dpy, int *return_screen_num) {
 	XGetWindowAttributes(dpy, win, &win_attr);
 
 	int nmonitors = 0;
-	XRRMonitorInfo *mon_inf = XRRGetMonitors(dpy, win, 1, &nmonitors);
+	XRRGetMonitors(dpy, win, 1, &nmonitors);
 
 	for (int i = 0; i < nmonitors; i++) {
 		XRRCrtcInfo *screen_info =
@@ -87,7 +75,7 @@ int get_screen_number_of_win(Window win, Display *dpy, int *return_screen_num) {
 	return 1;
 }
 
-int get_coord_for_corner(Window win, int corner, int *return_x,
+int get_coord_for_corner(Window win, double rel_x, double rel_y, int *return_x,
 	int *return_y) {
 
 	XWindowAttributes win_attrib;
@@ -99,7 +87,6 @@ int get_coord_for_corner(Window win, int corner, int *return_x,
 		printf("Could not find the screen the given window resides in.\n");
 		return 1;
 	}
-
 	/* If the given window could not be found, return 1 */
 	if (window_exists(win)) {
 		printf("Could not find window of given id.\n");
@@ -114,38 +101,10 @@ int get_coord_for_corner(Window win, int corner, int *return_x,
 	/* 0 to get the first monitor */
 	screen_info = XRRGetCrtcInfo(dpy, screen_res, screen_res->crtcs[win_belong_to]);
 
-	if (corner == TOP_LEFT) {
-		*return_x = screen_info->x;
-		*return_y = screen_info->y;
-	} else if (corner == TOP_CENTER) {
-		*return_x = (screen_info->width)/2 - (win_attrib.width)/2 + screen_info->x;
-		*return_y = screen_info->y;
-	} else if (corner == TOP_RIGHT) {
-		*return_x = (screen_info->width) - (win_attrib.width) + screen_info->x;
-		*return_y = screen_info->y;
-	} else if (corner == MIDDLE_LEFT) {
-		*return_x = screen_info->x;
-		*return_y = (screen_info->height)/2 - (win_attrib.height)/2 + screen_info->y;
-	} else if (corner == MIDDLE_CENTER) {
-		*return_x = (screen_info->width)/2 - (win_attrib.width)/2 + screen_info->x;
-		*return_y = (screen_info->height)/2 - (win_attrib.height)/2 + screen_info->y;
-	} else if (corner == MIDDLE_RIGHT) {
-		*return_x = (screen_info->width) - (win_attrib.width) + screen_info->x;
-		*return_y = (screen_info->height)/2 - (win_attrib.height)/2 + screen_info->y;
-	} else if (corner == BOTTOM_LEFT) {
-		*return_x = screen_info->x;
-		*return_y = (screen_info->height) - (win_attrib.height) + screen_info->y;
-	} else if (corner == BOTTOM_CENTER) {
-		*return_x = (screen_info->width)/2 - (win_attrib.width)/2 + screen_info->x;
-		*return_y = (screen_info->height) - (win_attrib.height) + screen_info->y;
-	} else if (corner == BOTTOM_RIGHT) {
-		*return_x = (screen_info->width) - (win_attrib.width) + screen_info->x;
-		*return_y = (screen_info->height) - (win_attrib.height) + screen_info->y;
-	// No valid corner number was given, return 1
-	} else {
-		printf("No valid corner number given. Must match [1-9].\n");
-		return 1;
-	}
+	*return_x = (rel_x)*(screen_info->width)
+		- (rel_x)*(win_attrib.width) + screen_info->x;
+	*return_y = (rel_y)*(screen_info->height)
+		- (rel_y)*(win_attrib.height) + screen_info->y;
 
 	return 0;
 
@@ -158,62 +117,92 @@ int get_coord_for_corner(Window win, int corner, int *return_x,
 */
 int main(int argc, char **argv) {
 
-	int corner = -1;
-	/* If the call to wmvc has 3 WORDS (wmvc, the corner number, and the
-	 * window id)
-	 * In regex form, wmvc [1-9] 0x[0-F]\{8\} */
-	if (argc == 3) {
+	int use_x = 0;
+	int use_y = 0;
+	double relative_x = 0;
+	double relative_y = 0;
+	unsigned long int win_id;
+	int use_anchors = 0;
+	int offset_borders = 0;
+	int print_results = 1;
+	int move_after = 0;
+	int ret_x = 0;
+	int ret_y = 0;
+	
+	int opt;
+	struct option opt_table[] = {
+		/* relative to screen arguments */
+		{ "x-ratio",		required_argument,	NULL,	'x' },
+		{ "y-ratio",		required_argument,	NULL,	'y' },
+		/* window id argument */
+		{ "window-id",		required_argument,	NULL,	'w' },
+		/* fine functionality options */
+		{ "use-anchor",		no_argument,	NULL,	'a' },
+		{ "offset-border",	no_argument,	NULL,	'b' },
+		{ "no-print",		no_argument,	NULL,	'N' },
+		{ "move-window",	no_argument,	NULL,	'm' },
+		{ 0, 0, 0, 0 }
+	};
 
-		if (!strcmp(argv[1], "1")) {
-			corner = TOP_LEFT;
-		} else if (!strcmp(argv[1], "2")) {
-			corner = TOP_CENTER;
-		} else if (!strcmp(argv[1], "3")) {
-			corner = TOP_RIGHT;
-		} else if (!strcmp(argv[1], "4")) {
-			corner = MIDDLE_LEFT;
-		} else if (!strcmp(argv[1], "5")) {
-			corner = MIDDLE_CENTER;
-		} else if (!strcmp(argv[1], "6")) {
-			corner = MIDDLE_RIGHT;
-		} else if (!strcmp(argv[1], "7")) {
-			corner = BOTTOM_LEFT;
-		} else if (!strcmp(argv[1], "8")) {
-			corner = BOTTOM_CENTER;
-		} else if (!strcmp(argv[1], "9")) {
-			corner = BOTTOM_RIGHT;
-		} else {
-			printf("No valid corner number given. Must match [1-9].\n");
-			print_help();
-			return 1;
+	while ((opt = getopt_long(argc, argv, \
+		"x:y:w:abNm", opt_table, NULL)) != -1) {
+		switch (opt) {
+			/* relative to screen options */
+			case 'x':
+				use_x = 1;
+				/* check if optarg is 0-1 */
+				relative_x = strtod(&optarg[0], NULL);
+				break;
+			case 'y':
+				use_y = 1;
+				relative_y = strtod(&optarg[0], NULL);
+				break;
+			/* window id argument */
+			case 'w':
+				win_id = strtoul(&optarg[0], NULL, 0);
+				break;
+			/* fine functionality options */
+			case 'a': use_anchors = 1; break;
+			case 'b': offset_borders = 1; break;
+			case 'N': print_results = 0; break;
+			case 'm': move_after = 1; break;
 		}
-
-		Display *dpy = XOpenDisplay(":0");
-		unsigned long int win_id = strtoul(&argv[2][0], NULL, 0);
-
-		int ret_x = 0;
-		int ret_y = 0;
-
-		int stat = get_coord_for_corner(win_id, corner, &ret_x, &ret_y);
-
-		if (stat != 0) {
-			return 1;
-		}
-
-		XWindowAttributes win_attrib;
-		XGetWindowAttributes(dpy, win_id, &win_attrib);
-
-		printf("%d %d %d %d 0x%08x\n", ret_x, ret_y, win_attrib.width, \
-			win_attrib.height, win_id);
-		return 0;
-
 	}
-	/* If the program wasn't called with 2 arguments */
-	else {
-		printf("Incorrect number of arguments provided.\n");
+
+	printf("past opt\n");
+
+	Display *dpy = XOpenDisplay(0);
+	XWindowAttributes win_attrib;
+	XGetWindowAttributes(dpy, win_id, &win_attrib);
+
+	/* Check if the program was given valid relative x and relative y values */
+	if (relative_x > 1 || relative_x < 0) {
+		printf("Relative x value provided was not within 0 <= value <= 1\n");
 		print_help();
-		return 1;
-	}
+		exit(1);
+	} else if (relative_y > 1 || relative_y < 0) {
+		printf("Relative y value provided was not within 0 <= value <= 1\n");
+		print_help();
+		exit(1);
+	/* If the program was given valid parameters for those variables */
+	} else {
 
-	return 0;
+		if (get_coord_for_corner(win_id, relative_x, relative_y,
+			&ret_x, &ret_y)) {
+			exit(1);
+		}
+
+		/* option falgs that take effect near the end of the process */
+		if (print_results == 1) {
+			printf("%d %d %d %d 0x%08x\n", ret_x, ret_y, win_attrib.width, \
+				win_attrib.height, win_id);
+		}
+		if (move_after == 1) {
+			// XMoveResizeWindow(dpy, win_id, ret_x, ret_y, win_attrib.width, \
+			// 	win_attrib.height);
+			XMoveResizeWindow(dpy, 0x02400009, 10, 10, 500, 500);
+		}
+
+		return 0;
+	}
 }
